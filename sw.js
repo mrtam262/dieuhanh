395a3caa943073fa9560ea524297b13ea7064bdf
mrtam262 <mr.tam262@gmail.com>
2026-08-03 09:08:06 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * Mỗi khi sửa index.html, hãy tăng số phiên bản PHIEN_BAN bên dưới (v1 -> v2 -> ...)
  * để điện thoại nhận bản mới thay vì dùng bản cũ đã lưu.
  */
-const PHIEN_BAN = 'ttdh-v5';
+const PHIEN_BAN = 'ttdh-v6';
 const VO_GIAO_DIEN = [
   './',
   './index.html',
